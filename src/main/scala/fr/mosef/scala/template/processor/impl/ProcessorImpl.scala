ab167cfb6340ebc97c4de2fc128ec35d649feda5
Léa Benovici <lea.benovici@gmail.com>
2024-04-08 20:27:43 +0200

    update

---
+++ src/main/scala/fr/mosef/scala/template/processor/impl/ProcessorImpl.scala
@@ -0,0 +1,13 @@
+package fr.mosef.scala.template.processor.impl
+
+
+import fr.mosef.scala.template.processor.Processor
+import org.apache.spark.sql.DataFrame
+
+class ProcessorImpl() extends Processor {
+
+  def process(inputDF: DataFrame): DataFrame = {
+    inputDF.groupBy("group_key").sum("field1")
+  }
+
+}
